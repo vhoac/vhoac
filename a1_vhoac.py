f3cb894d96efe56ff6a083f2b56073fd3172382e
vhoac <77449719+vhoac@users.noreply.github.com>
2021-02-24 17:30:42 -0500

    Add files via upload
    Removed template elements from Assignment 1 script, updated sanitize() function and range_check() function

--- a1_vhoac.py
+++ a1_vhoac.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-''' template for ops435 assignment 1 script
-    put your script level docstring here...
-    you can have more than one line of docstring.
-    Please personlize the following author declaration:
+'''
 -----------------------------------------------------------------------
 OPS435 Assignment 1 - Winter 2021
 Program: a1_vhoac.py
@@ -39,7 +36,7 @@ def sanitize(obj1,obj2):
     ''' 
     results = ''
     for char in user_raw_data:
-        if char in '0123456789':
+        if char in allow_chars:
             results += char
     return results
 
@@ -68,12 +65,12 @@ def range_check(obj1, obj2):
     '''
     status = ''
     if year >= 1900 and year <= 9999:
-        try:
-            if month in days_in_month.keys():
-                if day in days_in_month.values():    
+        if month in days_in_month.keys():
+            try:
+                if day in days_in_month.values():
                     status = True
-        except KeyError:
-            status = False
+            except KeyError:
+                status = False
     else:
         status = False
     return status
@@ -87,29 +84,23 @@ def usage():
     return status
 
 if __name__ == "__main__":
-   # step 1
    if len(sys.argv) != 2:
       print(usage())
       sys.exit()
-   # step 2
    month_name = ['Jan','Feb','Mar','Apr','May','Jun',
                  'Jul','Aug','Sep','Oct','Nov','Dec']
    days_in_month = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30,
                     7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
    user_raw_data = sys.argv[1]
-   # step 3
    allow_chars = '0123456789'
    dob = sanitize(user_raw_data, allow_chars)
-   # step 4
    result = size_check(dob,8)
    if result == False:
        print("Error 09: wrong date entered")
        sys.exit()
-   # step 5
    year = int(dob[0:4])
    month = int(dob[4:6])
    day = int(dob[6:])
-   # step 6
    result = range_check(year,(1900,9999))
    if result == False:
        print("Error 10: year out of range, must be 1900 or later")
@@ -125,7 +116,5 @@ if __name__ == "__main__":
    if result == False:
        print("Error 03: wrong day entered")
        sys.exit()
-   # step 7
    new_dob = str(month_name[month - 1])+' '+ str(day)+', '+str(year)
-   # step 8
-   print(new_dob)  
+   print(new_dob)
